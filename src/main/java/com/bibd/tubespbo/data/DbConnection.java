40a92a1a0c5ce72d6fc61003c700706c4e6580fc
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-06-06 12:03:32 +0700

    added database connection and combo box data model

--- src/main/java/com/bibd/tubespbo/data/DbConnection.java
+++ src/main/java/com/bibd/tubespbo/data/DbConnection.java
@@ -4,14 +4,68 @@
  */
 package com.bibd.tubespbo.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author asthiseta
  */
 public class DbConnection {
     
-    // Ayam
-    // bebek
+        Connection connection;
+
+    public void openConnection() {
+        try {
+
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/perpus",
+                    "bebek", "bebekgoyeng"
+            );
+            System.out.println("Connection to SQL has been established.");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int executeStatement(String query) {
+        int resultcount = 0;
+        Statement stmnt;
+        try {
+            stmnt = connection.createStatement();
+            resultcount = stmnt.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return resultcount;
+    }
+
+    public ResultSet getData(String query) {
+        Statement stmnt;
+        ResultSet rs = null;
+
+        try {
+            stmnt = connection.createStatement();
+            rs = stmnt.executeQuery(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return rs;
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
     
     
 }
