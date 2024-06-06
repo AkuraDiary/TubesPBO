8638da8d70a1f5d82c4521795aea696df554cb30
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-06-06 10:22:26 +0700

    first commit

---
+++ src/main/java/com/bibd/tubespbo/TubesPBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bibd.tubespbo;
+
+/**
+ *
+ * @author asthiseta
+ */
+public class TubesPBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
