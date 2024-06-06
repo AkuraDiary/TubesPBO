b3a5e23fdea1c927e417fafb0034b2c18c11391c
Rapid-Rawr <yagabut4@gmail.com>
2024-06-06 11:48:47 +0700

    loginnya

---
+++ src/main/java/com/bibd/tubespbo/rafid.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bibd.tubespbo;
+
+/**
+ *
+ * @author asthiseta
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("yanto ganteng");
+    }
+    
+}
