39dd827e0bf75d74e6439ace275633b0c4d06587
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-06-07 20:09:50 +0700

    added statics for easy and concise statusess

---
+++ src/main/java/com/bibd/tubespbo/util/Statics.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bibd.tubespbo.util;
+
+/**
+ *
+ * @author asthiseta
+ */
+public class Statics {
+    
+    public static String EMPLOYEE_STATUS_AKTIF = "aktif";
+    public static String EMPLOYEE_STATUS_NONAKTIF = "nonaktif";
+    
+    public static String EMPLOYEE_ROLE_MANAGER = "manager";
+    public static String EMPLOYEE_ROLE_SUPERVISOR = "supervisor";
+    public static String EMPLOYEE_ROLE_SALES = "sales";
+    
+    public static String ORDER_TYPE_PENJUALAN = "penjualan";
+    public static String ORDER_TYPE_Pembelian = "pembelian";
+    
+    public static String SHIPMENT_STATUS_PENDING = "pending";
+    public static String SHIPMENT_STATUS_SHIPPEd = "shipped";
+    
+    public static String PEMBELIAN_STATUS_PENDING = "pending";
+    public static String PEMBELIAN_STATUS_RUNNING = "running";
+    public static String PEMBELIAN_STATUS_FINNISH = "finnish";
+    
+    
+}
