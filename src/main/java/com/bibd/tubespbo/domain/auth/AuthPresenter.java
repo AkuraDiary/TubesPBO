6719d54959efd80f0bbf1fe96969f27eda40c7e5
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-06-07 19:10:31 +0700

    setup domain

---
+++ src/main/java/com/bibd/tubespbo/domain/auth/AuthPresenter.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bibd.tubespbo.domain.auth;
+
+import com.bibd.tubespbo.data.model.EmployeeModel;
+
+/**
+ *
+ * @author asthiseta
+ */
+public class AuthPresenter {
+    EmployeeModel loggedInUser;
+    public void doLogout(){
+        // TODO
+    }
+    
+    public void doLogin(String email, String password){
+        // TODO
+    }
+}
--- src/main/java/com/bibd/tubespbo/domain/manager/ManageEmployeePresenter.java
+++ src/main/java/com/bibd/tubespbo/domain/manager/ManageEmployeePresenter.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.bibd.tubespbo.domain;
+package com.bibd.tubespbo.domain.manager;
 
 /**
  *
- * @author rafid
+ * @author asthiseta
  */
-public class sales {
+public class ManageEmployeePresenter {
     
 }
