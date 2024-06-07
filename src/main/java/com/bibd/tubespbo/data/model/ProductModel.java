01c00961d5b7e08d9a94258925a2f935ed51ba1d
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-06-07 15:55:17 +0700

    added product data source

---
+++ src/main/java/com/bibd/tubespbo/data/model/ProductModel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bibd.tubespbo.data.model;
+
+/**
+ *
+ * @author asthiseta
+ */
+public class ProductModel {
+    int idProduct, categoryId, produsenId;
+    long buyPrice, sellPrice;
+    String productName, description;
+}
---
+++ src/main/java/com/bibd/tubespbo/data/source/ProductDataSource.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bibd.tubespbo.data.source;
+
+import com.bibd.tubespbo.data.DbConnection;
+
+/**
+ *
+ * @author asthiseta
+ */
+public class ProductDataSource {
+    
+    DbConnection db;
+
+    public ProductDataSource(DbConnection db) {
+        this.db = db;
+    }
+    
+    
+}
