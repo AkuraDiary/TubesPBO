80aae3c317a25e5ec799588ee7fb4f611a405638
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-06-10 10:22:52 +0700

    wayah e wong tulus tampil

--- src/main/java/com/bibd/tubespbo/data/source/ProductDataSource.java
+++ src/main/java/com/bibd/tubespbo/data/source/ProductDataSource.java
@@ -71,15 +71,20 @@ public class ProductDataSource {
     }
 
     public int updateProductStock(int idproduct, int jumlah, int idEmployee) {
+        
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     public int insertProduct(String productName, int quantityInStock, long buyPrice, long sellPrice, int categoryId, int produsenId) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+        
+        
     }
 
     public int insertCategory(String namaCategory, String descCategory) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+        
+        
     }
 
     public ArrayList<CategoryModel> getAllCategory() {
