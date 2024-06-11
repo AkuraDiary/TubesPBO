06b2913dfef0071f04ea0bae36885e446fc6eca8
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-06-11 18:49:32 +0700

    crud customer done bang

--- src/main/java/com/bibd/tubespbo/view/manager/panels/MenuCrudEmployees.java
+++ src/main/java/com/bibd/tubespbo/view/manager/panels/MenuCrudEmployees.java
@@ -573,7 +573,7 @@ public class MenuCrudEmployees extends javax.swing.JPanel {
     }//GEN-LAST:event_tFSearchEmployeeActionPerformed
 
     private void tViewdataemployeesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tViewdataemployeesMouseClicked
-        // TODO add your handling code here:
+       
         int row = tViewdataemployees.getSelectedRow();
 
         int idEmployee = Integer.parseInt(tableEmployeeModel.getValueAt(row, 0).toString());
